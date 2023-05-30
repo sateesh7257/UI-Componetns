536bf8495fb69430da278d8f9d554b0725538bc0
Chinta Sateesh Kumar <sateesh.kumar@valuelabs.com>
2023-05-30 14:28:56 +0530

    Updated js

--- packages/checkbox/build/index.js
+++ packages/checkbox/build/index.js
@@ -3,11 +3,8 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Checkbox", {
-  enumerable: true,
-  get: function get() {
-    return _Checkbox["default"];
-  }
-});
+exports["default"] = void 0;
 var _Checkbox = _interopRequireDefault(require("./Checkbox"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _default = _Checkbox["default"];
+exports["default"] = _default;
